75a6694824edd7303129809861d85c1b4041a8ef
Samba <samba@samba.samba.local>
2020-03-21 18:22:01 -0400

    Fix app to work with postgre

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,7 +12,7 @@ from resources.user import UserRegister
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ. get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.secret_key = "sam"
 api = Api(app)
